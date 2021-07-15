6c879b2182fee586b6c67e6eeea0e54ac53c2845
P1aer <arhoangel@bk.ru>
2021-07-15 20:56:44 +0300

    feat: add setting for hosting

--- english-for-kids/backend/app.js
+++ english-for-kids/backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const config = require("config")
+const path = require("path")
 const mongoose = require("mongoose")
 
 const app = express()
@@ -11,6 +12,14 @@ app.use("/api/auth", require("./routes/auth.route"))
 app.use("/api/category", require("./routes/category.route"))
 
 app.use("/api/word", require("./routes/word.route"))
+
+if (process.env.NODE_ENV === "production"){
+  app.use("/", express.static(path.join(__dirname, "../front", "build")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../front", "build", " index.html"))
+  })
+}
 
 const PORT = config.get("port") || 5000
 
